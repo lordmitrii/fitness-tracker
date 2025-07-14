e8b714447b66418498ee3ad3d2e9ea5b6282e25f
Dmitrii <dmitrylor@gmail.com>
2025-07-14 14:54:08 +0100

    Add service worker and manifest

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App.jsx";
 
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({ immediate: true });
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,64 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifest = {
+  theme_color: "#ffffff",
+  background_color: "#ffffff",
+  icons: [
+    {
+      purpose: "maskable",
+      sizes: "512x512",
+      src: "icon512_maskable.png",
+      type: "image/png",
+    },
+    {
+      purpose: "any",
+      sizes: "512x512",
+      src: "icon512_rounded.png",
+      type: "image/png",
+    },
+  ],
+  screenshots: [
+    {
+      src: "/screenshots/desktop.png",
+      type: "image/png",
+      sizes: "1883x903",
+      form_factor: "wide",
+    },
+    {
+      src: "/screenshots/mobile.png",
+      type: "image/png",
+      sizes: "366x795",
+      form_factor: "narrow",
+    },
+  ],
+  orientation: "any",
+  display: "standalone",
+  dir: "auto",
+  lang: "en",
+  name: "Fitness Tracker",
+  short_name: "FTracker",
+  start_url: "/",
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,jpg,svg}"],
+      },
+      manifest: manifest,
+    }),
+  ],
   server: {
     proxy: {
       "/api": {
